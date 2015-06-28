f4a3ae0c10b5040b5b9940367036f976775a0884
https://github.com/ryook/code.git <ryoo777@gmail.com>
2015-06-28 23:41:37 +0900

    form登録成功まで

--- ourtimes.py
+++ ourtimes.py
@@ -6,7 +6,7 @@ from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 from contextlib import closing
 import datetime
-from entries.models import Entries
+from entries.models import Entries, Links, Columns, Statics
 from entries.database import db_session
 
 
@@ -67,9 +67,11 @@ def show_entries():
 @app.route("/detail/<id>", methods=["GET"])
 def show_detail(id):
 	entry = Entries.query.filter_by(id=id).first()
+	links = Links.query.filter_by(entry_id=id).first()
 	if entry is None:
 		abort(404)
-	return render_template("detail.html",entry=entry)
+	print entry.__getattribute__
+	return render_template("detail.html",entry=entry,links=links)
 
 @app.route("/detail/<id>", methods=["POST"])
 def edit_detail(id=None):
@@ -86,25 +88,46 @@ def edit_detail(id=None):
     return redirect(url_for("detail.html",entry=content))
 
 
+
 @app.route("/new", methods=["GET", "POST"])
 def new():
 	return render_template("post.html")
 
 @app.route("/add", methods=["POST"])
 def add_entry():
+	print request.form
 	# message = ""
 	if not session.get("logged_in"):
 		abort(401)
 	d = datetime.datetime.today()
 	date = datetime.date(d.year, d.month, d.day)
-	# g.db.execute("insert into entries (title, member, date) values (?, ?, ?)",
-	# 				[request.form["title"], request.form["member"], date])
-	# g.db.commit()
 	entry = Entries(request.form["title"],request.form["member"],date)
+	column1 = Columns(request.form["column1"],request.form["c1_image"])
+	column2 = Columns(request.form["column2"],request.form["c2_image"])
+	columns = [column1, column2]
+	entry.Columns = columns
+	statics = [Statics(request.form["statics"],request.form["s_image"],request.form["s_link"])]
+	entry.Statics = statics
+	link1 = Links(request.form["news1"],request.form["n1_link"])
+	link2 = Links(request.form["news2"],request.form["n2_link"])
+	link3 = Links(request.form["news3"],request.form["n3_link"])
+	link4 = Links(request.form["news4"],request.form["n4_link"])
+	link5 = Links(request.form["news5"],request.form["n5_link"])
+	link6 = Links(request.form["news6"],request.form["n6_link"])
+	link7 = Links(request.form["news7"],request.form["n7_link"])
+	link8 = Links(request.form["news8"],request.form["n8_link"])
+	link9 = Links(request.form["news9"],request.form["n9_link"])
+	link10 = Links(request.form["news10"],request.form["n10_link"])
+	links = [link1, link2, link3, link4, link5, link6, link7, link8, link9, link10]
+	entry.links = links
 	db_session.add(entry)
 	db_session.commit()
 	flash("New entry was successfully posted")
 	return redirect(url_for("show_entries"))
+
+# def add_link():
+# 	id = Entries.query.filter_by(id=id).first()
+
 
 
 if __name__ == '__main__':
