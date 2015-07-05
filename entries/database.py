f3334805eb4ab226704509b1517d30ef03f8143a
https://github.com/ryook/code.git <ryoo777@gmail.com>
2015-07-06 03:32:57 +0900

    add migrate

--- entries/database.py
+++ entries/database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
 import os
 
 # databese_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'ourtimes.db')
@@ -12,6 +14,14 @@ db_session = scoped_session(sessionmaker(autocommit=False,
 Base = declarative_base()
 Base.query = db_session.query_property()
 
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
 def init_db():
     import entries.models
     Base.metadata.create_all(bind=engine)
+
+if __name__ == '__main__':
+	manager.run()
